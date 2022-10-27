f6ed30a5f4f536e5a648d10b2affcb4600e3c090
Allan Jardine <allan@datatables.net>
2022-10-27 15:13:27 +0000

    Update typing information to latest typing integration for DataTables

---
+++ types/types.d.ts
@@ -0,0 +1,21 @@
+
+// Dist-DataTables-KeyTable-DataTables integration with DataTables exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-KeyTable-DataTables integration with DataTables exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-KeyTable-DataTables integration with DataTables exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
