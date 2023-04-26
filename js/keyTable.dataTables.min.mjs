56a075079db07fb96b4411327e22820fb8a433e4
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-04-26 12:00:53 +0100

    546faf027caea51584622fb222041d46ac37113c New: `-event key-prefocus` which can be used to cancel a focus action
    Sync to source repo @546faf027caea51584622fb222041d46ac37113c

--- js/keyTable.dataTables.min.mjs
+++ js/keyTable.dataTables.min.mjs
@@ -1,4 +1,4 @@
 /*! DataTables styling wrapper for KeyTable
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-dt";import KeyTable from"datatables.net-keytable";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-dt";import KeyTable from"datatables.net-keytable";let $=jQuery;export default DataTable;
